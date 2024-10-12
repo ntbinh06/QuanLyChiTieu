b34e9f445b627e8957278a00bd89ed5ef8caffec
NhanDuc <ducn72999@gmail.com>
2024-10-12 23:09:12 +0700

    duc

--- app/src/main/java/com/example/quanlychitieu/Controller/ChiTietNganSach.java
+++ app/src/main/java/com/example/quanlychitieu/Controller/ChiTietNganSach.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.quanlychitieu.R;
 
-public class NganSachMoi extends AppCompatActivity {
+public class ChiTietNganSach extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_ngan_sach_moi);
+        setContentView(R.layout.activity_chi_tiet_ngan_sach);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
