8f9af6469296a116ddf02be69935e34b4f2cb1ce
ntbinh06 <binhqb2004@gmail.com>
2024-12-19 22:29:50 +0700

    binh

--- app/src/main/java/com/example/quanlychitieu/View/Fragment_NguoiDung.java
+++ app/src/main/java/com/example/quanlychitieu/View/Fragment_NguoiDung.java
@@ -20,7 +20,6 @@ import com.example.quanlychitieu.Controller.Ctrl_ThongTinCaNhan;
 import com.example.quanlychitieu.Controller.Ctrl_TongQuan;
 import com.example.quanlychitieu.Controller.Ctrl_TrangChuDNDK;
 import com.example.quanlychitieu.R;
-import com.google.common.base.FinalizableReference;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -28,35 +27,28 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.bumptech.glide.Glide;
-
 
 public class Fragment_NguoiDung extends Fragment {
 
     private ImageView ivUserAvatar, ic_back;
     private Button btnDangXuat, btnXoaTaiKhoan;
-    private FirebaseFirestore firestore;
-    private String userId;
     private TextView txtHeaderTenUser, txtHeaderEmailUser, txtTenUser, txtEamil, txtDateBirth, txtSDT;
     private LinearLayout thongtinCN;
 
     public Fragment_NguoiDung() {
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout cho Fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.activity_nguoi_dung, container, false);
 
         ic_back = view.findViewById(R.id.ic_back);
-        ImageView thongtin= view.findViewById(R.id.thongtin);
-        ImageView ic_doimk= view.findViewById(R.id.ic_doimk);
+        ImageView thongtin = view.findViewById(R.id.thongtin);
+        ImageView ic_doimk = view.findViewById(R.id.ic_doimk);
         LinearLayout layoutThongTinCaNhan = view.findViewById(R.id.thongtinCN);
-        // Ánh xạ View
+
+        // Initialize TextViews
         txtHeaderTenUser = view.findViewById(R.id.heade_user_name);
         txtHeaderEmailUser = view.findViewById(R.id.hd_user_email);
         txtTenUser = view.findViewById(R.id.user_name);
@@ -93,130 +85,97 @@ public class Fragment_NguoiDung extends Fragment {
                     Toast.makeText(getActivity(), "Không thể tải dữ liệu", Toast.LENGTH_SHORT).show();
                 }
             });
-          }
-        ic_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), Ctrl_TongQuan.class));
-            }
-        });
-        thongtin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), Ctrl_ThongTinCaNhan.class));
-            }
-        });
-        ic_doimk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), Ctrl_DoiMatKhau.class));
-            }
-        });
+        }
 
-        layoutThongTinCaNhan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getActivity(), Ctrl_ThongTinCaNhan.class));
-            }
-        });
+        // Set up click listeners
+        ic_back.setOnClickListener(v -> startActivity(new Intent(getActivity(), Ctrl_TongQuan.class)));
+        thongtin.setOnClickListener(v -> startActivity(new Intent(getActivity(), Ctrl_ThongTinCaNhan.class)));
+        ic_doimk.setOnClickListener(v -> startActivity(new Intent(getActivity(), Ctrl_DoiMatKhau.class)));
+        layoutThongTinCaNhan.setOnClickListener(v -> startActivity(new Intent(getActivity(), Ctrl_ThongTinCaNhan.class)));
 
+        // Logout button
         btnDangXuat = view.findViewById(R.id.btnDangXuat);
-        btnDangXuat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Tạo LayoutInflater để tạo view từ layout tùy chỉnh
-                LayoutInflater inflater = LayoutInflater.from(getContext());
-                View dialogView = inflater.inflate(R.layout.custom_dialog_dangxuat, null);
+        btnDangXuat.setOnClickListener(v -> showLogoutDialog());
 
-                // Khởi tạo AlertDialog
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                builder.setView(dialogView); // Set layout tùy chỉnh
-
-                // Khởi tạo các thành phần trong layout tùy chỉnh
-                TextView tvMessage = dialogView.findViewById(R.id.tvMessage);
-                Button btnLogout = dialogView.findViewById(R.id.btnLogout);
-                Button btnCancel = dialogView.findViewById(R.id.btnCancel);
-
-                // Tạo AlertDialog
-                AlertDialog dialog = builder.create();
-                dialog.getWindow().setBackgroundDrawableResource(R.drawable.border_dialog); // Đặt nền cho dialog
-
-                // Xử lý sự kiện cho nút Đăng xuất
-                btnLogout.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // Xử lý đăng xuất và điều hướng về trang đăng nhập
-                        navigateToLoginScreen();
-                        dialog.dismiss(); // Đóng dialog sau khi thực hiện đăng xuất
-                    }
-                });
-
-                // Xử lý sự kiện cho nút Hủy
-                btnCancel.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // Đóng hộp thoại, không làm gì cả
-                        dialog.dismiss();
-                    }
-                });
-
-                // Hiển thị AlertDialog
-                dialog.show();
-            }
-        });
-
+        // Delete account button
         btnXoaTaiKhoan = view.findViewById(R.id.btn_xoataikhoan);
-        btnXoaTaiKhoan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Tạo LayoutInflater để tạo view từ layout tùy chỉnh
-                LayoutInflater inflater = LayoutInflater.from(getContext());
-                View dialogView = inflater.inflate(R.layout.custom_dialog_xoataikhoan, null);
-
-                // Khởi tạo AlertDialog
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                builder.setView(dialogView); // Set layout tùy chỉnh
-
-                // Khởi tạo các thành phần trong layout tùy chỉnh
-                TextView tvMessage = dialogView.findViewById(R.id.tvMessage);
-                Button btnDelete = dialogView.findViewById(R.id.btnDelete);
-                Button btnCancel = dialogView.findViewById(R.id.btnCancel);
-
-                // Tạo AlertDialog
-                AlertDialog dialog = builder.create();
-                dialog.getWindow().setBackgroundDrawableResource(R.drawable.border_dialog); // Đặt nền cho dialog
-
-                // Xử lý sự kiện cho nút Đăng xuất
-                btnDelete.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // Xử lý đăng xuất và điều hướng về trang đăng nhập
-                        navigateToLoginScreen();
-                        dialog.dismiss(); // Đóng dialog sau khi thực hiện đăng xuất
-                    }
-                });
-
-                // Xử lý sự kiện cho nút Hủy
-                btnCancel.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // Đóng hộp thoại, không làm gì cả
-                        dialog.dismiss();
-                    }
-                });
-
-                // Hiển thị AlertDialog
-                dialog.show();
-            }
-        });
-
-
+        btnXoaTaiKhoan.setOnClickListener(v -> showDeleteAccountDialog());
 
         return view;
     }
 
+    private void showLogoutDialog() {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View dialogView = inflater.inflate(R.layout.custom_dialog_dangxuat, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setView(dialogView);
+
+        TextView tvMessage = dialogView.findViewById(R.id.tvMessage);
+        Button btnLogout = dialogView.findViewById(R.id.btnLogout);
+        Button btnCancel = dialogView.findViewById(R.id.btnCancel);
+
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.border_dialog);
+
+        btnLogout.setOnClickListener(v -> {
+            navigateToLoginScreen();
+            dialog.dismiss();
+        });
+
+        btnCancel.setOnClickListener(v -> dialog.dismiss());
+
+        dialog.show();
+    }
+
+    private void showDeleteAccountDialog() {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View dialogView = inflater.inflate(R.layout.custom_dialog_xoataikhoan, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setView(dialogView);
+
+        Button btnDelete = dialogView.findViewById(R.id.btnDelete);
+        Button btnCancel = dialogView.findViewById(R.id.btnCancel);
+
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.border_dialog);
+
+        btnDelete.setOnClickListener(v -> {
+            deleteUserAccount();
+            dialog.dismiss();
+        });
+
+        btnCancel.setOnClickListener(v -> dialog.dismiss());
+
+        dialog.show();
+    }
+
+    private void deleteUserAccount() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String userId = user.getUid();
+            user.delete().addOnCompleteListener(deleteTask -> {
+                if (deleteTask.isSuccessful()) {
+                    DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("NguoiDung").child(userId);
+                    userRef.removeValue().addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getActivity(), "Xóa tài khoản và dữ liệu thành công", Toast.LENGTH_SHORT).show();
+                            navigateToLoginScreen();
+                        } else {
+                            String errorMessage = task.getException() != null ? task.getException().getMessage() : "Lỗi không xác định";
+                            Toast.makeText(getActivity(), "Xóa dữ liệu người dùng không thành công: " + errorMessage, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    String errorMessage = deleteTask.getException() != null ? deleteTask.getException().getMessage() : "Lỗi không xác định";
+                    Toast.makeText(getActivity(), "Xóa tài khoản không thành công: " + errorMessage, Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(getActivity(), "Không có người dùng đăng nhập", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private void navigateToLoginScreen() {
         startActivity(new Intent(getActivity(), Ctrl_TrangChuDNDK.class));
-
     }
 }
